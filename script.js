9fbf8ac4ed64a5ee069b8712959f361b31a4dbf7
Wagner Ghedin <wagnerghedin10@gmail.com>
2023-12-29 14:27:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  gracefulStop: '30s',
+  stages: [
+    { target: 20, duration: '15s' },
+    { target: 20, duration: '1m' },
+    { target: 0, duration: '15s' },
+  ],
+};
+
+export default function() {
+  http.get('https://marsair.recruiting.thoughtworks.net/WagnerGhedin');
+  sleep(1);
+}
